dffb45cc798d4911d26ee5ab14a11ab3d00ad1a7
Matheus <matheuscauandeoliveira@gmail.com>
2024-11-20 16:29:20 -0300

    mudanças de texto

--- script-pantera.js
+++ script-pantera.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkAnswers() {
     const correctAnswers = ["T'Challa", "Wakanda", "Dora Milaje", "Vibranium", "Stan Lee e Jack Kirby", "Erik Killmonger", "Fantastic Four", "Superforça e sentidos aprimorados", "Shuri", "Capitão América: Guerra Civil"];
     let userAnswers = [];
@@ -23,29 +22,3 @@ function checkAnswers() {
 
     resultsDiv.innerHTML += `<h2>Você acertou ${score} de 10 perguntas!</h2>`;
 }
-=======
-function checkAnswers() {
-    const correctAnswers = ["T'Challa", "Wakanda", "Dora Milaje", "Vibranium", "Stan Lee e Jack Kirby", "Erik Killmonger", "Fantastic Four", "Superforça e sentidos aprimorados", "Shuri", "Capitão América: Guerra Civil"];
-    let userAnswers = [];
-    let form = document.forms["quizForm"];
-    let resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = ""; // Limpa os resultados anteriores
-
-    for (let i = 1; i <= 10; i++) {
-        let answer = form["q" + i].value;
-        userAnswers.push(answer);
-    }
-
-    let score = 0;
-    for (let i = 0; i < 10; i++) {
-        if (userAnswers[i] === correctAnswers[i]) {
-            score++;
-            resultsDiv.innerHTML += `<p>Pergunta ${i + 1}: Correto!</p>`;
-        } else {
-            resultsDiv.innerHTML += `<p>Pergunta ${i + 1}: Incorreto. A resposta certa é "${correctAnswers[i]}".</p>`;
-        }
-    }
-
-    resultsDiv.innerHTML += `<h2>Você acertou ${score} de 10 perguntas!</h2>`;
-}
->>>>>>> fbd73d258a6192384fb495ed17e4d67cc7cb8125
--- script-spider.js
+++ script-spider.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkAnswers() {
     const correctAnswers = ["Miles Morales", "Nova York", "Camuflagem", "Aaron Davis", "Peter Parker", "Universo Ultimate", "Brian Michael Bendis", "Ganke Lee", "Rei do Crime", "Eletricidade"];
     let userAnswers = [];
@@ -23,29 +22,6 @@ function checkAnswers() {
 
     resultsDiv.innerHTML += `<h2>Você acertou ${score} de 10 perguntas!</h2>`;
 }
-=======
-function checkAnswers() {
-    const correctAnswers = ["Miles Morales", "Nova York", "Camuflagem", "Aaron Davis", "Peter Parker", "Universo Ultimate", "Brian Michael Bendis", "Ganke Lee", "Rei do Crime", "Eletricidade"];
-    let userAnswers = [];
-    let form = document.forms["quizForm"];
-    let resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = ""; // Limpa os resultados anteriores
 
-    for (let i = 1; i <= 10; i++) {
-        let answer = form["q" + i].value;
-        userAnswers.push(answer);
-    }
 
-    let score = 0;
-    for (let i = 0; i < 10; i++) {
-        if (userAnswers[i] === correctAnswers[i]) {
-            score++;
-            resultsDiv.innerHTML += `<p>Pergunta ${i + 1}: Correto!</p>`;
-        } else {
-            resultsDiv.innerHTML += `<p>Pergunta ${i + 1}: Incorreto. A resposta certa é "${correctAnswers[i]}".</p>`;
-        }
-    }
 
-    resultsDiv.innerHTML += `<h2>Você acertou ${score} de 10 perguntas!</h2>`;
-}
->>>>>>> fbd73d258a6192384fb495ed17e4d67cc7cb8125
